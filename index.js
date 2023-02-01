49d20099bce0ce61301627a0b519b2846b0c4320
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-02-01 15:01:32 -0500

    Built function to view employess

--- index.js
+++ index.js
@@ -87,3 +87,14 @@ function viewRole() {
         startApp();
     });
 }
+
+function viewEmployee() {
+    let query = "SELECT * FROM employee";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    })
+}
+
+
