d3edd7ae5900e2e8f950502d57e4d13d28fc0c1e
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-02-01 14:41:59 -0500

    Built view departments function

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
-//
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const table = require('console.table');
+require('dotenv').config()
+
+const Connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.PASSWORD,
+        database: 'employee_db' 
+    },
+);
+
+startApp();
+
+function startApp() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'options',
+                message: "Choose from the following what you would like to do.",
+                choices: [
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add A Department',
+                    'Add A Role',
+                    'Add An Employee',
+                    'Update An Employee Role',
+                    'Exit'
+                ]
+            }
+        ])
+        .then(function ({options}) {
+            switch (options) {
+                case 'View All Departments':
+                    viewDepartment();
+                    break;
+
+                case 'View All Roles':
+                    viewRole();
+                    break;
+
+                case 'View All Employees':
+                    viewEmployee();
+                    break;
+
+                case 'Add A Department':
+                    addDepartment();
+
+                case 'Add A Role':
+                    addRole();
+                    break;
+
+                case 'Add An Employee':
+                    addEmployee();
+                    break;
+
+                case 'Update An Employee Roles':
+                    updateRole();
+                    break;
+
+                case 'Exit':
+                    Connection.end()
+                    return;
+            }
+        });
+
+}
+
+function viewDepartment() {
+    let query = "SELECT * FROM role";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
